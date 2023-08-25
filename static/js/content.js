dc6c36884e9e2a06d0a331b3faf980e8718705bc
mclovin <108329399+McLovin-RSPS@users.noreply.github.com>
2023-08-25 14:35:17 -0500

    Update content.js

--- static/js/content.js
+++ static/js/content.js
@@ -1,1 +1,6 @@
-!function(){"use strict";chrome.runtime.onMessage.addListener((function(e,n,t){"COPY_PAGE_TEXT"===e.type&&t(document.body.innerText)}))}();
+! function() {
+    "use strict";
+    chrome.runtime.onMessage.addListener((function(e, n, t) {
+        "COPY_PAGE_TEXT" === e.type && t(document.body.innerText)
+    }))
+}();
